6da2eba3d98232b1530618a2ae4ee60784bddef3
Dylain Cohen <dylain.cohen@wework.com>
2018-02-19 22:34:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,11 +8,11 @@ class Dog
   end
 
   def name = (dogs_name)
-    this_dogs_name = dogs_name
+    @this_dogs_name = dogs_name
   end
 
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 
 end
